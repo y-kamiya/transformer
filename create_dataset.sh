fa60de9273f0e2854e0d73f19bc64c49e7f97687
Yuji Kamiya <y.kamiya0@gmail.com>
2020-05-02 23:03:18 +0900

    script to prepare iwslt2015 dataset

--- create_dataset.sh
+++ create_dataset.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-name=$1
+data_dir=$1
 src=${2:-en}
 tgt=${3:-fr}
 vocab_size=${4:-1000}
-valid_count=${5:-1000}
+valid_count=${5:-0}
 max_words=${6:-1000000}
 
 script_dir=$(cd `dirname $0`; pwd)
-data_dir=$script_dir/data/$name
+model_prefix=spm
+
+pushd $data_dir
 
 function encode()
 {
-    lang=$1
-    data_dir=$2
-    name=$3
+    local lang=$1
+    local data_type=$2
+    local model_prefix=$3
 
-    cat $data_dir/train.orig.$lang \
-        | ./sentencepiece/build/src/spm_encode --model=$data_dir/$name.model --output_format=id \
+    cat $data_type.orig.$lang \
+        | $script_dir/sentencepiece/build/src/spm_encode --model=$model_prefix.model --output_format=id \
         | sed -e 's/^/1 /' -e 's/$/ 2/' \
-        > $data_dir/train.$lang
+        > $data_type.$lang
 }
 
 function remove_long_sentences()
 {
-    input_src=$1
-    input_tgt=$2
-    max_words=$3
+    local input_src=$1
+    local input_tgt=$2
+    local max_words=$3
 
     cat $input_src | awk  -v mw=$max_words 'mw < NF || NF <= 2 { print NR "d" }' > a.tmp
     cat $input_tgt | awk  -v mw=$max_words 'mw < NF || NF <= 2 { print NR "d" }' >> a.tmp
@@ -43,32 +45,43 @@ function remove_long_sentences()
 
 function separate()
 {
-    lang=$1
-    data_dir=$2
-    valid_count=$3
+    local lang=$1
+    local valid_count=$2
 
     # separate data for train and valid
-    head -n $valid_count $data_dir/train.$lang > $data_dir/valid.$lang
-    sed -e "1,${valid_count}d" $data_dir/train.$lang > $data_dir/a.tmp
+    head -n $valid_count train.$lang > valid.$lang
+    sed -e "1,${valid_count}d" train.$lang > a.tmp
 
     # use tmp file to avoid using 'sed -i' because usage is different between GNU and BSD
-    mv $data_dir/a.tmp $data_dir/train.$lang
+    mv a.tmp train.$lang
 }
 
-pushd $script_dir
+cat train.orig.$src train.orig.$tgt > spm_input
+$script_dir/sentencepiece/build/src/spm_train --input=spm_input --model_prefix=$model_prefix --vocab_size=$vocab_size --pad_id=3
 
-cat $data_dir/train.orig.$src $data_dir/train.orig.$tgt > $data_dir/spm_input
-./sentencepiece/build/src/spm_train --input=$data_dir/spm_input --model_prefix=$name --vocab_size=$vocab_size --pad_id=3
+function build()
+{
+    local data_type=$1
+    encode $src $data_type $model_prefix
+    encode $tgt $data_type $model_prefix
 
-mv $name.model $name.vocab $data_dir/
+    remove_long_sentences $data_type.$src $data_type.$tgt $max_words
+}
 
-encode $src $data_dir $name
-encode $tgt $data_dir $name
+build train
 
-remove_long_sentences $data_dir/train.$src $data_dir/train.$tgt $max_words
+if [ -f valid.orig.$src ]; then
+    build valid
+fi
+if [ -f test.orig.$src ]; then
+    build test
+fi
 
-separate $src $data_dir $valid_count
-separate $tgt $data_dir $valid_count
+# separate valid data from train data
+if [ $valid_count != 0 ]; then
+    separate $src $valid_count
+    separate $tgt $valid_count
+fi
 
 popd
 
